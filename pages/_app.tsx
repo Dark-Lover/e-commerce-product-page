2dc86c1955133d1a2bde4999502da5e3cdaffa53
Dark-Lover <person.zen15@gmail.com>
2022-07-15 11:18:08 +0100

    update title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Hello</title>
+        <title>E-commerce Product Page | Houssine</title>
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
           rel="stylesheet"
